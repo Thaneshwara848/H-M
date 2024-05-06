66eee9e01084193d91038ebc98cd9b069854e657
Thaneshwara848 <thaneshwaramanjunatha@gmail.com>
2024-05-06 15:52:03 +0530

    Chat code

--- Demo2.java
+++ Demo2.java
@@ -1,9 +1,3 @@
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Thaneshwara848/H-M.git
-git push -u origin main
 
 import java.util.Scanner;
 c lass Dev
